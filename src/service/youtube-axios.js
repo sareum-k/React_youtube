1ad10319a4eddae40a5ec10e46b60a25978a84e6
sareum <rkdtkfma@gmail.com>
2021-12-10 09:31:38 +0900

    change fetch to axios

---
+++ src/service/youtube-axios.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+class Youtube {
+    constructor(key) {
+        this.youtube = axios.create({
+            baseURL: 'https://youtube.googleapis.com/youtube/v3',
+            params: { key: key },
+        })
+    }
+
+    async mostPopular() {
+        const response = await this.youtube.get('videos', { //'video'는 v3/뒤에 들어가는 단어
+            params: {
+                part: 'snippet',
+                chart: 'mostPopular',
+                maxResults: 25
+            }
+        })
+        return response.data.items;
+    }
+
+    async search(query) {
+        const response = await this.youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 25,
+                q: query,
+                type: 'video'
+            }
+        })
+        return response.data.items.map(item => ({ ...item, id: item.id.videoId }))
+    }
+}
+
+export default Youtube;
